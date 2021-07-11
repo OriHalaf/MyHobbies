c4e7e15e279d5947a0ac16009bb041d508c70dbe
marik14 <marik040792@gmail.com>
2021-07-11 19:12:02 +0300

    ...marik

--- app/src/main/java/com/example/myhobbies/paint.java
+++ app/src/main/java/com/example/myhobbies/paint.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class paint extends AppCompatActivity {
-
+    public static int num = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
