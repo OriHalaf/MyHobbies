58e2cf446fad0cfff933d0e6e37f9ef1d1d3ae3d
marik14 <marik040792@gmail.com>
2021-07-11 19:15:27 +0300

    ...marik

--- app/src/main/java/com/example/myhobbies/paint.java
+++ app/src/main/java/com/example/myhobbies/paint.java
@@ -9,5 +9,6 @@ public class paint extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
+        //דשגשדגדשג
     }
 }
